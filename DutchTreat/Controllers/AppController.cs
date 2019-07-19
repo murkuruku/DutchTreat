338b36d204e53d263fac2f3dfa785a2025689311
Mukuruku <berteskalukas@gmail.com>
2019-07-19 03:07:26 +0300

    1night

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,51 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+           _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        
+        [HttpGet("contact")]
+        public IActionResult Contacts()
+        {
+           
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contacts(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("berteskalukas@gmail.com", model.Subject, $"Form:{model.Name} - {model.Email}, Message: {model.Message}, ");
+                ModelState.Clear();
+            }
+            ViewBag.UserMessage = "Message sent";
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,51 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DutchContext>(cfg => {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionStribg"));
+            });
+            services.AddTransient<IMailService, NullMailService>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+             app.UseDeveloperExceptionPage();
+            }else
+            {
+                app.UseExceptionHandler("/error");
+            }
+            app.UseNodeModules(env);
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Foo",
+                    "/{controller}/{action}/{id?}", 
+                    new {controller ="App", Action = "Index"} 
+                    );
+            });
+        }
+    } 
+}
